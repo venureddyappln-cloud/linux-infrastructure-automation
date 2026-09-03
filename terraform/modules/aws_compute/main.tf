4f9a4689eba6a00129d14b698d3cf05999a1e17d
VenuGopal Reddy <venureddyappln@gmail.com>
2026-09-03 12:04:12 -0500

    Fix formatting in main.tf for AWS provider version

--- terraform/modules/aws_compute/main.tf
+++ terraform/modules/aws_compute/main.tf
@@ -3,7 +3,7 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-          version = "~> 5.0"
+      version = "~> 5.0"
     }
   }
 }
@@ -28,4 +28,5 @@ resource "aws_security_group" "linux_core" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 }
+
 
